e7fbdcfd44ae6f1ff5042a03347b2ce52a66c1b1
LiuTao <mat.liutos@gmail.com>
2013-02-05 10:57:58 +0800

    Fix RSS bug, rename some functions and change the type of last modified time of instance of class POST.

--- src/rss.lisp
+++ src/rss.lisp
@@ -1,16 +1,14 @@
 (in-package :cl-github-page)
 
 (defun rss/posts-datum (posts)
-  ;; (assert (typep posts 'array))
-  (map 'list
-       #'(lambda (post)
-           (list
-            :post-title (post-title post)
-            :post-url (post-url post)
-            :post-date (post-date post)
-            :post-id (post-id post)
-            :post-content (post-content post)))
-       posts))
+  (mapcar #'(lambda (post)
+              (list
+               :post-title (post-title post)
+               :post-url (post-url post)
+               :post-date (post-date post)
+               :post-id (post-id post)
+               :post-content (post-content post)))
+          posts))
 
 (defun write-rss (posts-list)
   (write-file-by-tmpl *atom*
