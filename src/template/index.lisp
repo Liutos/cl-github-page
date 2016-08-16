7254918c7da49b2489a1fa821ee48f3fc264e06a
Liutos <mat.liutos@gmail.com>
2016-08-16 22:57:11 +0800

    支持填充首页模板的功能

--- src/template/index.lisp
+++ src/template/index.lisp
@@ -1,5 +1,18 @@
 (in-package #:com.liutos.cl-github-page.template)
 
+(defun fill-index-template (blog-description
+                            blog-title
+                            categories
+                            posts)
+  (let ((template #p"/media/world2/liutos/src/cl/cl-github-page/src/template/tmpl/index.html")
+        (values (list :blog-description blog-description
+                      :blog-title blog-title
+                      :categories categories
+                      :posts posts)))
+    (with-output-to-string (string)
+      (html-template:fill-and-print-template template values
+                                             :stream string))))
+
 (defun fill-post-template (blog-description
                            blog-title
                            categories
