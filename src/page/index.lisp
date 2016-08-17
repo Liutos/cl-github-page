18f1cfaefa918475d7abbde2590c26a3b92fdb78
Liutos <mat.liutos@gmail.com>
2016-08-17 22:19:22 +0800

    简化页面模块中将模板填充后的内容写入文件的代码

--- src/page/index.lisp
+++ src/page/index.lisp
@@ -1,45 +1,39 @@
 (in-package #:com.liutos.cl-github-page.page)
 
+(defvar *blog-description* "")
+(defvar *blog-title* "Liutos的博客")
+
 (defun write-index-page ()
-  (let ((blog-description "")
-        (blog-title "Liutos的博客")
+  (let ((blog-description *blog-description*)
+        (blog-title *blog-title*)
         (categories '())
-        html
         (post-list (com.liutos.cl-github-page.storage:get-post-list))
         posts)
     (setf posts
           (mapcar #'(lambda (post)
                       (list :post-title (getf post :title)))
                   post-list))
-    (setf html
-          (com.liutos.cl-github-page.template:fill-index-template blog-description
-                                                                  blog-title
-                                                                  categories
-                                                                  posts))
-    (with-open-file (file (format nil "/tmp/index.html")
-                          :direction :output
-                          :if-exists :supersede)
-      (write-string html file))))
+    (com.liutos.cl-github-page.template:fill-index-template blog-description
+                                                            blog-title
+                                                            categories
+                                                            posts
+                                                            :destination #P"/tmp/index.html")))
 
 (defun write-post-page (post-id)
   (let ((post (com.liutos.cl-github-page.storage:find-post post-id)))
     (unless post
       (error "~D: 文章不存在" post-id))
-    (let ((blog-description "")
-          (blog-title "Liutos的博客")
+    (let ((blog-description *blog-description*)
+          (blog-title *blog-title*)
           (categories '())
-          html
+          (destination (pathname (format nil "/tmp/~D.html" post-id)))
           (post-body (getf post :body))
           (post-meta (getf post :create_at))
           (post-title (getf post :title)))
-      (setf html
-            (com.liutos.cl-github-page.template:fill-post-template blog-description
-                                                                   blog-title
-                                                                   categories
-                                                                   post-body
-                                                                   post-meta
-                                                                   post-title))
-      (with-open-file (file (format nil "/tmp/~D.html" post-id)
-                            :direction :output
-                            :if-exists :supersede)
-        (write-string html file)))))
+      (com.liutos.cl-github-page.template:fill-post-template blog-description
+                                                             blog-title
+                                                             categories
+                                                             post-body
+                                                             post-meta
+                                                             post-title
+                                                             :destination destination))))
--- src/template/index.lisp
+++ src/template/index.lisp
@@ -1,24 +1,45 @@
 (in-package #:com.liutos.cl-github-page.template)
 
+(defgeneric fill-template-and-print (destination template values))
+
+(defmethod fill-template-and-print ((destination (eql nil)) template values)
+  (with-output-to-string (string)
+    (fill-template-and-print string template values)))
+
+(defmethod fill-template-and-print ((destination pathname) template values)
+  (with-open-file (file destination
+                        :direction :output
+                        :if-exists :supersede)
+    (fill-template-and-print file template values)))
+
+(defmethod fill-template-and-print ((destination stream) template values)
+  (html-template:fill-and-print-template template
+                                         values
+                                         :stream destination))
+
+;;; EXPORT
+
 (defun fill-index-template (blog-description
                             blog-title
                             categories
-                            posts)
+                            posts
+                            &key
+                              destination)
   (let ((template #p"/media/world2/liutos/src/cl/cl-github-page/src/template/tmpl/index.html")
         (values (list :blog-description blog-description
                       :blog-title blog-title
                       :categories categories
                       :posts posts)))
-    (with-output-to-string (string)
-      (html-template:fill-and-print-template template values
-                                             :stream string))))
+    (fill-template-and-print destination template values)))
 
 (defun fill-post-template (blog-description
                            blog-title
                            categories
                            post-body
                            post-meta
-                           post-title)
+                           post-title
+                           &key
+                             destination)
   (let ((html-template:*string-modifier* #'identity)
         (template #p"/media/world2/liutos/src/cl/cl-github-page/src/template/tmpl/post.html")
         (values (list :blog-description blog-description
@@ -27,6 +48,4 @@
                       :post-body post-body
                       :post-meta post-meta
                       :post-title post-title)))
-    (with-output-to-string (string)
-      (html-template:fill-and-print-template template values
-                                             :stream string))))
+    (fill-template-and-print destination template values)))
