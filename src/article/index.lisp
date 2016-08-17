ccb99f8bb9dcc6bd766b9aca856634ee052aa8d4
Liutos <mat.liutos@gmail.com>
2016-08-17 13:06:56 +0800

    实现修改数据库中的文章的功能

--- src/article/index.lisp
+++ src/article/index.lisp
@@ -15,3 +15,20 @@
                                                    1
                                                    source
                                                    title)))
+
+(defun modify-post (post-id
+                    source
+                    title)
+  (when (pathnamep source)
+    (if (com.liutos.cl-github-page.file:is-file-exists source)
+        (setf source (com.liutos.cl-github-page.file:get-file-content source))
+        (error "~A: 文件不存在" source)))
+  (when (com.liutos.cl-github-page.storage:find-post-by-source source)
+    (error "存在一模一样的文章"))
+  (let (body)
+    (setf body
+          (com.liutos.cl-github-page.compile:compile-from-markdown source))
+    (com.liutos.cl-github-page.storage:update-post post-id
+                                                   :body body
+                                                   :source source
+                                                   :title title)))
